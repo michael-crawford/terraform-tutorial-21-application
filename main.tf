da886ee0668e624b87b80f3b517d4f91bcff8b16
Michael Crawford <michael.crawford@neudesic.com>
2022-07-01 01:45:14 -0700

    Add remaining Security Groups and Bastion

--- main.tf
+++ main.tf
@@ -54,6 +54,10 @@ data "terraform_remote_state" "vpc" {
     command = "echo VPC created on `date` and VPC ID: ${data.terraform_remote_state.vpc.outputs.vpc_id} >> creation-time-vpc-id.txt"
 */
 
+################################################################################
+# Security Groups
+################################################################################
+
 module "bastion_instance_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.0.0"
@@ -70,3 +74,97 @@ module "bastion_instance_security_group" {
 
   tags = local.common_tags
 }
+
+module "application_instance_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.0.0"
+
+  name        = "${var.environment}-${var.application}-InstanceSecurityGroup"
+  description = "${var.environment}-${var.application}-InstanceSecurityGroup"
+
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress_rules       = ["ssh-tcp", "http-80-tcp", "http-8080-tcp"]
+  ingress_cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
+
+  egress_rules        = ["all-all"]
+
+  tags = local.common_tags
+}
+
+module "application_balancer_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.0.0"
+
+  name        = "${var.environment}-${var.application}-BalancerSecurityGroup"
+  description = "${var.environment}-${var.application}-BalancerSecurityGroup"
+
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+
+  tags = local.common_tags
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 81
+      to_port     = 81
+      protocol    = 6
+      description = "Allow Port 81 from internet"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
+
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-hvm-*-gp2" ]
+  }
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+
+data "aws_route53_zone" "domain" {
+  name         = var.domain
+}
+
+resource "aws_key_pair" "administrator" {
+  key_name   = var.instance_keypair
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDU9mdeb+HCafxD0dwedSxZDJyzTrLz/lAGS8gTYrP4qYLmIWziKGv4d3Bd5JYk6sWL5yfVCm1NUdkzczcbygM1Q4HrQ6Xmj++gOyGyGPynNy3Unn9lWk5OVdG3zIDT/Qet84Tsxyjpmh/4hhL8ZM0SaDPF1S6ra825Q0vYbLOH4u8/88gruzNrjrU00RCQ/YQtIycqlOc/LiWQgFN7h6vrPU3V9fWtQxulh+SE+Yw+gkFjR61mmtjiZCi9hL2C29pwGcBOiXBJXGIwcxp2w/r5FiBXuqqT4cbfHO8olcwQ5m2kSUMbwcSOrJ3V0ciVbVaRsTfxVGmLsJCu4pAiZJviITPca2uBGjBHx4pW9syZMGRTZR+s8YwbGBMv2nNdmCc+MI0yElHuzWcoYHC7FiUm+MNrHDgfKOdToptPW+oKNTISjE4e03FaHn83bzHKGPqzDmv4lhI8WhS3t835ypwmCcpLn6DBUCLStY9578s3yjw5XXa5QVqcw1t2hnNPkRFQhmLSn6Qc6vwbkUprnSR6i0nnnbyRl+7v04N2ei2EhaykE8pXED9FUVLqyROiG6UBRPgcqC51oM3I5LmphfYYXL5MqClbfK06F9l2+rej0EEzlSv8Fm4SLRVD44vJkMiDObmDnQXiJQrILc2gZlSxfFF2JT4b5qAOTDnLahPjxw== administrator@mjcconsulting.com"
+}
+
+module "bastion" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.17.0"
+
+  name                   = "${var.environment}-Bastion-Instance"
+
+  instance_count         = 1
+  ami                    = data.aws_ami.amzn2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  monitoring             = true
+
+  subnet_id = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+
+  vpc_security_group_ids = [module.bastion_instance_security_group.security_group_id]
+
+  tags = local.common_tags
+}
+
--- outputs.tf
+++ outputs.tf
@@ -13,19 +13,37 @@ output "bastion_instance_security_group_name" {
   value       = module.bastion_instance_security_group.security_group_name
 }
 
-/*
-output "private_sg_group_id" {
+output "application_instance_security_group_id" {
   description = "The ID of the security group"
-  value       = module.private_sg.security_group_id
+  value       = module.application_instance_security_group.security_group_id
 }
 
-output "private_sg_group_vpc_id" {
+output "application_instance_security_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.private_sg.security_group_vpc_id
+  value       = module.application_instance_security_group.security_group_vpc_id
 }
 
-output "private_sg_group_name" {
+output "application_instance_security_group_name" {
   description = "The name of the security group"
-  value       = module.private_sg.security_group_name
+  value       = module.application_instance_security_group.security_group_name
 }
-*/
+
+output "domain_zoneid" {
+  description = "The Hosted Zone id of the desired Hosted Zone"
+  value       = data.aws_route53_zone.domain.zone_id
+}
+
+output "domain_name" {
+  description = " The Hosted Zone name of the desired Hosted Zone."
+  value       = data.aws_route53_zone.domain.name
+}
+
+output "bastion_ids" {
+  description = "List of IDs of instances"
+  value       = module.bastion.id
+}
+
+output "bastion_public_ip" {
+  description = "List of public IP addresses assigned to the instances"
+  value       = module.bastion.public_ip
+}
